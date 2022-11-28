0fcbf4b431e1db5671929d744be5a8802b5e6714
27tr <114559221+MCDream3@users.noreply.github.com>
2022-11-28 13:07:41 +0000

    Algorytmy NWD

---
+++ AlgorytmyNWD.py
@@ -0,0 +1,13 @@
+#NWD odejmowanie
+# a = int(input())
+# b = int(input())
+# while a!=b:
+#   if a>b : a=a-b
+#   if b>a : b=b-a
+# print(a)
+
+#NWD modulo :0
+# a,b = int(input()), int(input())
+# while b>0:
+#   a,b = b,a%b
+# print (a)
