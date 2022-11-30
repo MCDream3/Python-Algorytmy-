0895aa3901b7f3f19442374b3c45090a25b198dc
27tr <114559221+MCDream3@users.noreply.github.com>
2022-11-30 08:19:58 +0000

    dodano NWW

--- AlgorytmyNWD.py
+++ AlgorytmyNWD.py
@@ -11,3 +11,18 @@
 # while b>0:
 #   a,b = b,a%b
 # print (a)
+#NWW odejmowanie
+# a = int(input())
+# b = int(input())
+# iloczyn = a*b
+# while a != b:
+#   if a > b: a -= b
+#   if b > a: b -= a
+#   print(iloczyn//a)
+#NWW modulo :0
+# a = int(input())
+# b = int(input())
+# iloczyn = a*b
+# while b > 0:
+#   a, b = b, a%b
+# print(iloczyn//a)
