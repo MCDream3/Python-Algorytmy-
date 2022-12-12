9e0d899572b8824141fc99696c69c394ee8121f2
27tr <114559221+MCDream3@users.noreply.github.com>
2022-12-12 13:12:16 +0000

    zadania 1-5 z karty pracy 6A

---
+++ KartaPracy6A.py
@@ -0,0 +1,56 @@
+#zad 1
+
+# x = int(input())
+# wynik = 0
+# while x>0:
+#   wynik+= x%10
+#   x //=10
+# print(wynik)
+
+#zad 2
+
+# a = int(input())
+# temp=0
+# for i in range(2,a):
+#   if a%i == 0:
+#     temp += 1
+# if temp>3:
+#   print("chyba tak")
+# else:
+#   print("no nie")
+
+#zad 3
+
+# n = int(input())
+# suma = 0
+# for i in range(1,n):
+#   if n%i == 0:
+#     suma +=i
+# if suma == n:
+#    print("jest doskonała")
+# else:
+#   print("you idoit its not")
+
+#zad 4
+
+# x = int(input())
+# y = int(input())
+# while  x!=y:
+#   if x>y: x-=y
+#   if y>x:y-=x
+# if x == 1:
+#   print("liczby są względnie pierwsze")
+# else:
+#   print("liczby takie nie są")
+
+#zad 5
+
+m = int(input())
+for i in range(10,20):
+  x = m
+  y = i
+  while y>0:
+    x,y=y, x%y
+    NWD = x
+    if x == 1:
+      print(f"mamy parę :0 : {m},{i}")
